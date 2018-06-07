14681390c69ce566b6740e6d391777ff4aa4c397
Valheru100 <39316748+Valheru100@users.noreply.github.com>
2018-06-07 10:28:26 +0100

    Update Player.java

--- src/main/Player.java
+++ src/main/Player.java
@@ -2,11 +2,13 @@ package main;
 
 import java.util.ArrayList;
 
+//Represents a player
 public class Player
 {
 	private String name;
 	private ArrayList<Integer> rankedChoices = new ArrayList<Integer>();
 	
+	//Takes a line from the input file PlayerChoices.txt and plits it via the format name,choice1,choice2,choice3,choic4,choice5
 	public Player(String inputLine)
 	{
 		String[] splitInput = inputLine.split(",");
